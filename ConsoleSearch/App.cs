d91e2c09b32392e853ba00dc61f2e6ade6309338
Mathias Kristensen <mathiaskristensen52@gmail.com>
2023-02-28 20:46:38 +0100

    Moved and added more stuff

--- ConsoleSearch/App.cs
+++ ConsoleSearch/App.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Common;
+using Newtonsoft.Json;
 
 namespace ConsoleSearch
 {
@@ -7,15 +12,37 @@ namespace ConsoleSearch
     {
         public void Run()
         {
-            SearchLogic mSearchLogic = new SearchLogic(new Database());
+            //SearchLogic mSearchLogic = new SearchLogic(new Database());
             Console.WriteLine("Console Search");
-            
+
+            using HttpClient client = new();
+            client.BaseAddress = new Uri("http://localhost:9000/");
+
             while (true)
             {
                 Console.WriteLine("enter search terms - q for quit");
                 string input = Console.ReadLine() ?? string.Empty;
                 if (input.Equals("q")) break;
 
+                Task<string> task = client.GetStringAsync("api/search/" + input);
+                task.Wait();
+
+                string searchResult = task.Result;
+                SearchResult result = JsonConvert.DeserializeObject<SearchResult>(searchResult);
+
+                foreach (var ignored in result.IgnoredTerms)
+                {
+                    Console.WriteLine(ignored + "was ignored");
+                }
+
+                foreach (var resultDocument in result.Documents)
+                {
+                    Console.WriteLine(resultDocument.Id + ": " + resultDocument.Path + " - number of terms found: " +
+                                      resultDocument.NumberOfOccurrences);
+                }
+
+                Console.WriteLine("Found " + result.Documents.Count + " in " + result.ElapsedMilliseconds + " ms");
+                /*
                 var wordIds = new List<int>();
                 var searchTerms = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
                 foreach (var t in searchTerms)
@@ -51,6 +78,7 @@ namespace ConsoleSearch
                     idx++;
                 }
                 Console.WriteLine("Documents: " + docIds.Count + ". Time: " + used.TotalMilliseconds);
+                */
             }
         }
     }
