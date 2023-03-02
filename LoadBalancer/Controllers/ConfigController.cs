c9da00960e76d3d7d45c72bfb2b731ab890a5dfc
Christian Lindholm <christianlindholm@jubii.dk>
2023-03-02 12:36:39 +0100

    Added CacheController

---
+++ LoadBalancer/Controllers/ConfigController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoadBalancer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ConfigController : ControllerBase
+{
+    [HttpPost]
+    //Method here
+    
+}
---
+++ LoadBalancer/Controllers/LoadBalancerController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoadBalancer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoadBalancerController : ControllerBase
+{
+    
+}
