aeba1122ab37ae8e2a4e5d747321ada7316e65c3
julianswagger <juli55d3@easv365.dk>
2023-03-10 13:31:55 +0100

    Create LeastConnection.cs
    nice

---
+++ LoadBalancer/LoadBalancer/LeastConnection.cs
@@ -0,0 +1,50 @@
+﻿namespace LoadBalancer.LoadBalancer
+{
+    public class LeastConnection
+    {
+        private Dictionary<string, int> _activeConnections = new Dictionary<string, int>();
+        private List<string> _servers = new List<string>();
+
+
+        public LeastConnection (List<string> servers)
+        {
+            _servers = servers;
+
+            // initialize active connections count to 0 for each server
+            foreach (string server in servers)
+            {
+                _activeConnections[server] = 0;
+            }
+        }
+
+        public string GetServer()
+        {
+            //find server with least active conncetions
+            string selectedServer = "";
+            int minConnections = int.MaxValue;
+
+            foreach(string server in _servers)
+            {
+                int connections = _activeConnections[server];
+                if (connections > minConnections)
+                {
+                    selectedServer = server;
+                    minConnections = connections;
+                }
+            }
+
+            //increment active connections count for selected server
+            _activeConnections[selectedServer]++;
+            return selectedServer;
+        }
+
+        public void ReleaseServer(string server)
+        {
+            if (_activeConnections.ContainsKey(server))
+            {
+                _activeConnections[server]--;
+            }
+        }
+
+    }
+}
